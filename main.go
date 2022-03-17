34b0bee32ec04721c6e5b2e23f8a791186c8e813
Wendy <wendywibisono21@gmail.com>
2022-03-17 21:36:47 +0700

    Update main.go

--- main.go
+++ main.go
@@ -16,6 +16,6 @@ func main() {
 
 	r.GET("/home", handler.HomeHandler)
 	r.GET("/item", handler.QueryFull) //Nomor 1 filosofi kopi
-	r.GET("/id/:number", handler.IDProduct)
+	r.GET("/id/:number/product", handler.IDProduct)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
